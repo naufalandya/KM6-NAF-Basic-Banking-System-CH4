3f110e0332ca28ca355e766c4f39758c64397c1f
Naufal Andya Faiz <135545118+naufalandya@users.noreply.github.com>
2024-03-30 23:52:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 5599
 
-const userRoutes = require("./src/api/v1/routes/users");
+const allRoutes = require("./src/api/v1/routes/users");
 
 app.use(express.json());
 
-app.use("/api/v1", userRoutes);
+app.use("/api/v1", allRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ error: "Not Found" });
